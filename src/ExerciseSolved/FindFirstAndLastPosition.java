04fd0dffa91c95c8c6197ea2e082656b6bdd066b
TranvandatWhiteX <tranvandatdh012@gmail.com>
2023-10-09 16:07:27 +0700

    leetcode 35

--- src/ExerciseSolved/FindFirstAndLastPosition.java
+++ src/ExerciseSolved/FindFirstAndLastPosition.java
@@ -21,7 +21,13 @@ public class FindFirstAndLastPosition {
         if (count != 0) result[1] = count + result[0]-1;
         return result;
     }
-
+    // Leetcode 35
+    public int searchInsert(int[] nums, int target) {
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] == target || nums[i] > target) return i;
+        }
+        return nums.length;
+    }
     public static void main(String[] args) {
         int[] input = {5,7,8,8,8,8,8,8,8,10};
         int[] output = searchRange(input, 8);
